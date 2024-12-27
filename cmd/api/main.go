fc3da93be0edf8375c051da9fd69650d295970be
Matheus Fiorin <matheus.fiorin@nubank.com.br>
2024-12-27 14:42:08 -0300

    Remove Fatal when .env isn't present

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	router := mux.NewRouter()
